0d0f2ae6068dc6444708fc6c701a6724663d6782
ᴸᴿKyle <89089895+LRKyle@users.noreply.github.com>
2022-02-04 14:38:48 -0800

    Add files via upload

---
+++ Previous versions/2021-9-22.py
@@ -0,0 +1,317 @@
+import os
+import discord
+import random
+import requests
+import asyncio
+
+from discord.utils import get
+from discord import FFmpegPCMAudio
+from discord.ext import commands
+from youtube_dl import YoutubeDL
+
+#Metal Cup Drink
+client = discord.Client();
+bot = commands.Bot(command_prefix='!', case_insensitive=True)
+
+FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
+YDL_OPTIONS = {'format': 'bestaudio', 'noplaylist':'True'}
+
+
+splitResp = []
+songQueue = []
+songDuration = []
+numQueue = []
+
+DoujinMax = 371497;
+tagNum = 0;
+num = 0
+qNum = 0
+
+
+#a = qNum.array('d', [1.1, 3.5, 4.5])
+
+RandRep = ["I'm doing great {message.author.nick}, How about you?", "I'm doing amazing! You?", "I'm doing decently, how are you? ", "I'm doing good :smile:, you? ", "I am just living life, How have have you been doing?"]
+
+RandRepH = ["That's good to hear!", "That's nice to hear", "That's nice", "Ah, I'm happy to hear that!", "Damn, that's good"]
+
+RandRepS =["Really? That hurts to hear", "That's sad", ":damn:", "hold that, bozo", "**Damn**"]
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user} - (ID: {bot.user.id})')
+    print('------')
+
+    #Playing
+    #await bot.change_presence(activity = discord.Game(name="That gives Kyle errors"))
+
+    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="Kyle struggle with errors"))
+
+@bot.event
+async def on_message(message):
+  if message.author == client.user:
+    return
+
+  #Responses
+  msg = message.content
+  RandRep = [f"I'm doing great {message.author.nick}, How about you?", "I'm doing amazing! You? {message.author.nick}", "I'm doing decently, how are you? ", "I'm doing good :smile:, you? ", "I am just living life, How have have you been doing?"]
+
+  if msg.startswith('~How are'):
+    RandRepR = random.randint(0, 4)
+    await message.channel.send(RandRep[RandRepR]);
+    
+    if msg.startswith("I'm doing good") or msg.startswith("I'm good") or msg.startswith("I'm g") or msg.startswith('good'):
+      RandRepR = random.randint(0, 4)
+      await message.channel.send(RandRepH[RandRepR]);
+
+    if msg.startswith("~I'm doing bad") or msg.startswith("!I'm bad") or msg.startswith("!I'm b") or msg.startswith('bad') or msg.startswith('Terrible') or msg.startswith('unlucky'):
+      RandRepR = random.randint(0, 4)
+      await message.channel.send(RandRepS[RandRepR]);
+
+    if msg.startswith("~I miss "):
+      await message.channel.send(":monkey:")
+
+  if msg.startswith("top 1 sus") or msg.startswith("Top 1"):
+    await message.channel.send("Buggin, it's Joel")
+  
+  if msg.startswith("~Omori"):
+    await message.author.send('Go play Omori bro')
+
+  await bot.process_commands(message)
+
+  
+
+@bot.command(aliases=['doujinshi'])
+async def doujin(ctx, arg):
+  print("The user asked for doujin number " + arg + "\n")
+  if int(arg) > DoujinMax:
+    await ctx.channel.send('The number is too high! Redo the command!');
+
+  if int(arg) < DoujinMax  :
+    Embed = discord.Embed(
+    title = 'Doujinshi Deliverer!',
+    description = (f'[**Your doujin!**](https://nhentai.net/g/{arg}/)'),
+    color = discord.Color.from_rgb(255, 0, 0))
+    #Embed.set_image(url="https://c.tenor.com/7fuoqpm9DtwAAAAd/please-dont-bully-me-anime.gif")
+    await ctx.channel.send(embed = Embed)
+
+@bot.command(aliases=['RDoujin', 'randd', 'randdoujin', 'rdoujinishi'])
+async def rand(ctx):
+  RandDoujin = random.randint(0,369677)
+  print(f"The random doujin that the user got was {RandDoujin}")
+  Embed = discord.Embed(
+  title = 'Randomize Doujin!',
+  description = f'[:tada:**Your mysterious doujin!**:tada:](https://nhentai.net/g/{RandDoujin}/)',
+  color = discord.Color.from_rgb(101, 67, 33))
+  Embed.set_image(url="https://c.tenor.com/7fuoqpm9DtwAAAAd/please-dont-bully-me-anime.gif")
+  await ctx.channel.send(embed = Embed)
+
+@bot.command(aliases = ['RHentai', 'randhentai', 'randh'])
+async def hrand(ctx):
+  RandHentai = random.randint(0,99999999999999)
+  print(f"The random doujin that the user got was {RandHentai}")
+  Embed = discord.Embed(
+  title = 'Randomize Hanime Page!',
+  description = f'[:monkey:**Hanime randomized!**:monkey:](https://hanime.tv/browse/random?r={RandHentai})',
+  color = discord.Color.from_rgb(101, 67, 33))
+  Embed.set_image(url="https://c.tenor.com/Fl6m3zSv3XgAAAAd/monkey-spinning.gif")
+  await ctx.channel.send(embed = Embed)
+
+@bot.command(aliases = ['char', 'character'])
+async def tag(ctx, *, arg):
+    if ' ' in arg:
+      splitResp = arg.split(' ')
+      JoinedResp = ('+'.join(splitResp))
+      await ctx.channel.send(f'The tag/character you searched for is!\nhttps://nhentai.net/search/?q={JoinedResp}')
+    else:
+      await ctx.channel.send(f'The tag/character you searched for is!\nhttps://nhentai.net/search/?q={arg}')
+
+@bot.command()
+async def google(ctx, *,arg):
+    JoinedResp = arg
+    if " " in arg:
+      splitResp = arg.split(' ')
+      JoinedResp = ('+'.join(splitResp))
+    Embed = discord.Embed(
+    title = 'Your Google Search',
+    description = f'[**`Your search result!`**:mag_right:](https://www.google.com/search?q={JoinedResp}&ei=yAozYeGyIuyOwbkP34iDqAw&oq={JoinedResp}&gs_lcp=Cgdnd3Mtd2l6EAMyCgguELEDEEMQkwIyCwguELEDEMcBEKMCMggIABCABBCxAzIICC4QgAQQsQMyBQguEIAEMggIABCABBCxAzIICAAQgAQQsQMyCwguEIAEEMcBEK8BMggIABCABBCxAzIICAAQgAQQsQM6DggAEOoCELQCENkCEOUCOgcILhBDEJMCOgQIABBDOgQILhBDOhEILhCABBCxAxCDARDHARDRAzoOCC4QgAQQsQMQxwEQ0QM6EQguEIAEELEDEIMBEMcBEKMCOg4ILhCABBCxAxDHARCjAjoFCAAQgAQ6DQguEMcBENEDEEMQkwI6EAguELEDEIMBEMcBEKMCEEM6CgguELEDEIMBEEM6CAgAEIAEEMkDOgUIABCSAzoHCC4QsQMQQzoHCAAQsQMQQ0oECEEYAFDdyh9Y9dIfYOjUH2gBcAJ4AIABzwGIAZUHkgEFMi40LjGYAQCgAQGwAQjAAQE&sclient=gws-wiz&ved=0ahUKEwjhtqu60eTyAhVsRzABHV_EAMUQ4dUDCA4&uact=5)',
+    color = discord.Color.from_rgb(27, 30, 35))
+    await ctx.channel.send(embed = Embed)
+
+#Join Function
+@bot.command(aliases = ['j'])
+async def join(ctx):
+  connected = ctx.author.voice
+  voice = discord.utils.get(ctx.bot.voice_clients, guild=ctx.guild)
+  bot_connection = ctx.guild.voice_client
+  if connected and voice == None:
+    channel = ctx.author.voice.channel
+    await channel.connect()
+  if not voice == None:
+    await bot_connection.move_to(ctx.author.voice.channel)
+  if not connected:
+    await ctx.channel.send("ayo? yo ass ain't in the voice channel")
+    await ctx.channel.send("https://c.tenor.com/c6rLwFIEvDcAAAAC/touhou-b7touhou.gif")
+
+#Leave Function
+@bot.command(aliases = ['l'])
+async def leave(ctx):
+  if (ctx.guild.voice_client):#If in the VC 
+    await ctx.channel.send('Farewell!');
+    await ctx.channel.send('https://c.tenor.com/h240kSCuz2cAAAAS/hanako-yashiro.gif');
+    await ctx.guild.voice_client.disconnect()
+  else: 
+    await ctx.channel.send("I'm not currently in a voice channel, use the join(!join) command to make me join!")
+
+#DM Function
+@bot.command(pass_context=True, aliases=['message'])
+async def dm(ctx, user: discord.User, *, message=None):
+  message = message or "I'm watching. :eye::eye:"
+  await user.send(message)
+
+
+#Make it so you have to be in the call to use the play
+#Play Function
+@bot.command(name='play', aliases=['p'], description="Use Ears")
+async def play(ctx, *, query):
+  voice = get(bot.voice_clients, guild=ctx.guild)
+
+  if(voice == None):
+    await ctx.send("I'm in not currently in a VC!\nuse the join command to make me join!")
+  def search(query):
+    global info
+
+    with YoutubeDL({'format': 'bestaudio', 'noplaylist':'False'}) as ydl:
+      try: requests.get(query)
+      except: info = ydl.extract_info(f"ytsearch:{query}", download=False)['entries'][1]
+      else: info = ydl.extract_info(query, download=False)
+    return (info, info['formats'][0]['url'])    
+  video, source = search(query)
+
+  FFMPEG_OPTS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5',   'options': '-vn'}
+
+  if not voice.is_playing():
+    await ctx.send(f"Now playing: **{info['title']}**")
+    voice.play(FFmpegPCMAudio(source, **FFMPEG_OPTS), after=lambda e: print('done', e))
+    songQueue.append(info['title'])
+    songDuration.append(info['duration'])
+    global dur
+    dur = info['duration']
+    while not dur == 0:
+      await asyncio.sleep(1)
+      dur = dur - 1
+
+  else: 
+    await ctx.send(f"**{info['title']}** is added to the queue!")
+    songQueue.append(info['title'])
+    songDuration.append(info['duration'])
+    await asyncio.sleep(dur)
+    songQueue.pop(0)
+    songDuration.pop(0)
+    voice.play(FFmpegPCMAudio(source, **FFMPEG_OPTS), after=lambda e: print('done', e))
+    durTime = info['duration']
+    while not durTime == 0:
+      await asyncio.sleep(1)
+      durTime = durTime - 1
+    await ctx.send(f"Now playing: **{info['title']}**")
+
+  qNum+1
+
+@bot.command(aliases = ['d'])
+async def duration(ctx):
+  await ctx.send(info['duration'])
+
+#Queue Function
+@bot.command(aliases = ['q'])
+async def queue(ctx):
+  global qNum
+
+  voice = discord.utils.get(ctx.bot.voice_clients)
+  await ctx.send('`Song List:`')
+
+  def convert(secs):
+    secs = secs % (24 * 3600)
+    hours = secs // 3600
+    secs %= 3600
+    mins = secs // 60
+    secs %= 60
+    return "%02d:%02d:%02d" % (hours, mins, secs)
+  
+  for qNum,qTitle in enumerate(songQueue):
+    durTime = int(songDuration[qNum])
+    await ctx.send(f"`[{qNum + 1}] {qTitle} \n Duration: {convert(durTime)}`")
+
+      
+  if(voice == None):
+    await ctx.send("I'm in not currently in a VC!\n use the join command to make me join!")
+    return
+
+#Resume Function
+@bot.command()
+async def resume(ctx):
+  voice = discord.utils.get(bot.voice_clients, guild=ctx.guild)
+  if voice.is_paused():
+    voice.resume()
+  elif(voice == None):
+    await ctx.send("I'm in not currently in a VC!\n use the join command to make me join!")
+  else:
+    await ctx.send("The audio is not paused.") 
+
+
+#Pause Function
+@bot.command()
+async def pause(ctx):
+  voice = discord.utils.get(bot.voice_clients, guild=ctx.guild)
+  if voice.is_playing():
+    voice.pause()
+  elif(voice == None):
+    await ctx.send("I'm in not currently in a VC!\n use the join command to make me join!")
+  else:
+    await ctx.send("Currently no audio is playing.")
+
+#Skip Function - this is next #Stop Pop Play
+@bot.command(aliases = ['s'])
+async def skip(ctx, *,  arg): 
+  voice = discord.utils.get(bot.voice_clients, guild=ctx.guild)
+  if int(arg) == 0:
+    print('rip bozo')
+    voice.stop(songQueue[num])
+  if not arg:
+    print('You really are dumb my nigga, where is the arg?')
+  elif(voice == None):
+    await ctx.send("I'm in not currently in a VC!\n use the join command to make me join!")  
+  else:
+    songQueue.pop(int(arg))
+    if(int(arg) > len(songQueue)):
+      ctx.send("Fucc is wrong with you my nigga, damn")
+
+#Clear Function - Make it so when cleared, it stops dur too or it will cause an Already playing audio error
+@bot.command()
+async def clear(ctx):
+  voice = discord.utils.get(bot.voice_clients, guild=ctx.guild)
+  if(voice == None):
+    await ctx.send("I'm in not currently in a VC!\n use the join command to make me join!")
+    #return
+  voice.stop()
+  await ctx.send("I cleared all of the music")
+  songQueue.clear()
+  
+  
+
+bot.run(os.environ['token'])
+
+#Stylize/rework everything to make it cute and shit
+
+
+'''async def chnick(ctx, member: discord.Member, nick):
+    await member.edit(nick=nick)
+    await ctx.send(f'Nickname was changed for {member.mention} ')+'''
+
+'''Saved Embed example
+Embed = discord.Embed(
+    title = 'Your random doujin!',
+    description = f'[:tada:**Your mysterious doujin!**:tada:](https://nhentai.net/g/{RandDoujin}/)'),
+    color = discord.Color.from_rgb(101, 67, 33)
+  )
+  Embed.set_image(url="https://c.tenor.com/7fuoqpm9DtwAAAAd/please-dont-bully-me-anime.gif")
+
+  await ctx.channel.send(embed = Embed)'''
---
+++ Previous versions/~New Play Function.py
@@ -0,0 +1,272 @@
+import os
+import discord
+import random
+import requests
+import asyncio
+from discord.utils import get
+from discord import FFmpegPCMAudio
+from discord.ext import commands
+from youtube_dl import YoutubeDL
+
+#Metal Cup Drink
+client = discord.Client();
+bot = commands.Bot(command_prefix='!', case_insensitive=True)
+
+
+
+
+FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
+YDL_OPTIONS = {'format': 'bestaudio', 'noplaylist':'True'}
+
+DoujinMax = 371497;
+tagNum = 0;
+SplitResp = []
+num = 0
+
+Squeue = []
+queueNum = len(Squeue)
+trackNum = 0
+
+count = 0
+
+RandRep = ["I'm doing great {message.author.nick}, How about you?", "I'm doing amazing! You?", "I'm doing decently, how are you? ", "I'm doing good :smile:, you? ", "I am just living life, How have have you been doing?"]
+
+RandRepH = ["That's good to hear!", "That's nice to hear", "That's nice", "Ah, I'm happy to hear that!", "Damn, that's good"]
+
+RandRepS =["Really? That hurts to hear", "That's sad", ":damn:", "hold that, bozo", "**Damn**"]
+
+
+  
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user} - (ID: {bot.user.id})')
+    print('------')
+
+@bot.event
+async def on_message(message):
+  if message.author == client.user:
+    return
+
+  #Responses
+  msg = message.content
+  RandRep = [f"I'm doing great {message.author.nick}, How about you?", "I'm doing amazing! You? {message.author.nick}", "I'm doing decently, how are you? ", "I'm doing good :smile:, you? ", "I am just living life, How have have you been doing?"]
+
+  if msg.startswith('~How are'):
+    RandRepR = random.randint(0, 4)
+    await message.channel.send(RandRep[RandRepR]);
+    
+    if msg.startswith("I'm doing good") or msg.startswith("I'm good") or msg.startswith("I'm g") or msg.startswith('good'):
+      RandRepR = random.randint(0, 4)
+      await message.channel.send(RandRepH[RandRepR]);
+
+    if msg.startswith("~I'm doing bad") or msg.startswith("!I'm bad") or msg.startswith("!I'm b") or msg.startswith('bad') or msg.startswith('Terrible') or msg.startswith('unlucky'):
+      RandRepR = random.randint(0, 4)
+      await message.channel.send(RandRepS[RandRepR]);
+
+    if msg.startswith("~I miss "):
+      await message.channel.send(":monkey:")
+
+  if msg.startswith("top 1 sus"):
+    await message.channel.send("Buggin, it's Joel")
+  
+  if msg.startswith("~Omori"):
+    await message.author.send('Go play Omori bro')
+
+  await bot.process_commands(message)
+
+  
+
+@bot.command(aliases=['doujinshi'])
+async def doujin(ctx, arg):
+  print("The user asked for doujin number " + arg + "\n")
+  if int(arg) > DoujinMax:
+    await ctx.channel.send('The number is too high! Redo the command!');
+
+  if int(arg) < DoujinMax  :
+    Embed = discord.Embed(
+    title = 'Doujinshi Deliverer!',
+    description = (f'[**Your doujin!**](https://nhentai.net/g/{arg}/)'),
+    color = discord.Color.from_rgb(255, 0, 0))
+    #Embed.set_image(url="https://c.tenor.com/7fuoqpm9DtwAAAAd/please-dont-bully-me-anime.gif")
+    await ctx.channel.send(embed = Embed)
+
+@bot.command(aliases=['RDoujin', 'random', 'randdoujin', 'rdoujinishi'])
+async def rand(ctx):
+  RandDoujin = random.randint(0,369677)
+  print(f"The random doujin that the user got was {RandDoujin}")
+  Embed = discord.Embed(
+  title = 'Randomize Doujin!',
+  description = f'[:tada:**Your mysterious doujin!**:tada:](https://nhentai.net/g/{RandDoujin}/)',
+  color = discord.Color.from_rgb(101, 67, 33))
+  Embed.set_image(url="https://c.tenor.com/7fuoqpm9DtwAAAAd/please-dont-bully-me-anime.gif")
+  await ctx.channel.send(embed = Embed)
+
+@bot.command(aliases = ['char', 'character'])
+async def tag(ctx, *, arg):
+    if ' ' in arg:
+      SplitResp = arg.split(' ')
+      JoinedResp = ('+'.join(SplitResp))
+      await ctx.channel.send(f'The tag/character you searched for is!\nhttps://nhentai.net/search/?q={JoinedResp}')
+    else:
+      await ctx.channel.send(f'The tag/character you searched for is!\nhttps://nhentai.net/search/?q={arg}')
+
+@bot.command()
+async def google(ctx, *,arg):
+    JoinedResp = arg
+    if " " in arg:
+      SplitResp = arg.split(' ')
+      JoinedResp = ('+'.join(SplitResp))
+    Embed = discord.Embed(
+    title = 'Your Google Search',
+    description = f'[**`Your search result!`**:mag_right:](https://www.google.com/search?q={JoinedResp}&ei=yAozYeGyIuyOwbkP34iDqAw&oq={JoinedResp}&gs_lcp=Cgdnd3Mtd2l6EAMyCgguELEDEEMQkwIyCwguELEDEMcBEKMCMggIABCABBCxAzIICC4QgAQQsQMyBQguEIAEMggIABCABBCxAzIICAAQgAQQsQMyCwguEIAEEMcBEK8BMggIABCABBCxAzIICAAQgAQQsQM6DggAEOoCELQCENkCEOUCOgcILhBDEJMCOgQIABBDOgQILhBDOhEILhCABBCxAxCDARDHARDRAzoOCC4QgAQQsQMQxwEQ0QM6EQguEIAEELEDEIMBEMcBEKMCOg4ILhCABBCxAxDHARCjAjoFCAAQgAQ6DQguEMcBENEDEEMQkwI6EAguELEDEIMBEMcBEKMCEEM6CgguELEDEIMBEEM6CAgAEIAEEMkDOgUIABCSAzoHCC4QsQMQQzoHCAAQsQMQQ0oECEEYAFDdyh9Y9dIfYOjUH2gBcAJ4AIABzwGIAZUHkgEFMi40LjGYAQCgAQGwAQjAAQE&sclient=gws-wiz&ved=0ahUKEwjhtqu60eTyAhVsRzABHV_EAMUQ4dUDCA4&uact=5)',
+    color = discord.Color.from_rgb(27, 30, 35))
+    await ctx.channel.send(embed = Embed)
+
+#Join Function
+@bot.command(aliases = ['j'])
+async def join(ctx):
+  connected = ctx.author.voice
+  voice = discord.utils.get(ctx.bot.voice_clients, guild=ctx.guild)
+  bot_connection = ctx.guild.voice_client
+  if connected and voice == None:
+    channel = ctx.author.voice.channel
+    await channel.connect()
+  if not voice == None:
+      await bot_connection.move_to(ctx.author.voice.channel)
+  if not connected:
+    await ctx.channel.send("ayo? yo ass ain't in the voice channel")
+    await ctx.channel.send("https://c.tenor.com/c6rLwFIEvDcAAAAC/touhou-b7touhou.gif")
+
+#Leave Function
+@bot.command(aliases = ['l'])
+async def leave(ctx):
+  if (ctx.guild.voice_client):#If in the VC 
+    await ctx.channel.send('Farewell!');
+    await ctx.channel.send('https://c.tenor.com/h240kSCuz2cAAAAS/hanako-yashiro.gif');
+    await ctx.guild.voice_client.disconnect()
+  else: 
+    await ctx.channel.send("I'm not currently in a voice channel, use the join(!join) command to make me join!")
+
+#DM Function
+@bot.command(pass_context=True, aliases=['message'])
+async def dm(ctx, user: discord.User, *, message=None):
+  message = message or "I'm watching. :eye::eye:"
+  await user.send(message)
+
+def search(query):
+  with YoutubeDL({'format': 'bestaudio', 'noplaylist':'True'}) as ydl:
+    try: requests.get(query)
+    except: info = ydl.extract_info(f"ytsearch:{query}", download=False)['entries'][0]
+    else: info = ydl.extract_info(query, download=False)
+  return (info, info['formats'][0]['url'])
+
+#Play Function
+@bot.command(name='play', aliases=['sing','p'], description="Use Ears")
+async def play(ctx, *, query):
+  def search(query):
+    global info
+    #Solves a problem I'll explain later
+    with YoutubeDL({'format': 'bestaudio', 'noplaylist':'True'}) as ydl:
+      try: requests.get(query)
+      except: info = ydl.extract_info(f"ytsearch:{query}", download=False)['entries'][0]
+      else: info = ydl.extract_info(query, download=False)
+    return (info, info['formats'][0]['url'])    
+  search(query)
+  voice = discord.utils.get(ctx.bot.voice_clients)
+  if not voice.is_playing():
+    await ctx.send(f"Now playing: {info['title']}")
+    FFMPEG_OPTS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5',   'options': '-vn'}
+    video, source = search(query)
+    voice = get(bot.voice_clients, guild=ctx.guild)
+    voice.play(FFmpegPCMAudio(source, **FFMPEG_OPTS), after=lambda e: print('done', e))
+  else: 
+    await ctx.send("Your song has been added to the queue!") 
+    await asyncio.sleep(info['duration'])
+    FFMPEG_OPTS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5',   'options': '-vn'}
+    video, source = search(query)
+    voice = get(bot.voice_clients, guild=ctx.guild)
+    voice.play(FFmpegPCMAudio(source, **FFMPEG_OPTS), after=lambda e: print('done', e))
+
+
+  
+#Queue Function
+@bot.command(aliases = ['q'])
+async def queue(ctx):
+  #for queueNum in Squeue:
+  await ctx.send(f"There are {len(Squeue)} songs\n You should know the songs that you entered, idiot")
+
+#Resume Function
+@bot.command()
+async def resume(ctx):
+  voice = discord.utils.get(bot.voice_clients, guild=ctx.guild)
+  if voice.is_paused():
+    voice.resume()
+  elif(voice == None):
+    await ctx.send("I'm in not currently in a VC!\n use the join command to make me join!")
+  else:
+    await ctx.send("The audio is not paused.") 
+
+
+    
+#Skip Function
+@bot.command(aliases = ['s'])
+async def skip(ctx, *,  arg): 
+  voice = discord.utils.get(bot.voice_clients, guild=ctx.guild)
+  if int(arg) == 0:
+    print('rip bozo')
+    voice.stop(Squeue[num])
+  if not arg:
+    print('You really are dumb my nigga, where is the arg?')
+  elif(voice == None):
+    await ctx.send("I'm in not currently in a VC!\n use the join command to make me join!")  
+  else:
+    Squeue.pop(int(arg))
+    if(int(arg) > len(Squeue)):
+      ctx.send("Fucc is wrong with you my nigga, damn")
+
+
+
+#Pause Function
+@bot.command()
+async def pause(ctx):
+  voice = discord.utils.get(bot.voice_clients, guild=ctx.guild)
+  if voice.is_playing():
+    voice.pause()
+  elif(voice == None):
+    await ctx.send("I'm in not currently in a VC!\n use the join command to make me join!")
+  else:
+    await ctx.send("Currently no audio is playing.")
+
+#Clear Function
+@bot.command()
+async def clear(ctx):
+  voice = discord.utils.get(bot.voice_clients, guild=ctx.guild)
+  voice.stop()
+  await ctx.send("I cleared all of the music")
+  Squeue.clear()
+  await ctx.send(len(Squeue))
+  if(voice == None):
+    await ctx.send("I'm in not currently in a VC!\n use the join command to make me join!")
+  
+  
+
+
+bot.run(os.environ['token'])
+
+#Stylize/rework everything to make it cute and shit
+
+
+'''async def chnick(ctx, member: discord.Member, nick):
+    await member.edit(nick=nick)
+    await ctx.send(f'Nickname was changed for {member.mention} ')+'''
+
+
+'''Saved Embed example
+Embed = discord.Embed(
+    title = 'Your random doujin!',
+    description = f'[:tada:**Your mysterious doujin!**:tada:](https://nhentai.net/g/{RandDoujin}/)'),
+    color = discord.Color.from_rgb(101, 67, 33)
+  )
+  Embed.set_image(url="https://c.tenor.com/7fuoqpm9DtwAAAAd/please-dont-bully-me-anime.gif")
+
+  await ctx.channel.send(embed = Embed)'''
